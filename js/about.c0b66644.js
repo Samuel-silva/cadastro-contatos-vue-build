5c7b96ddacf852c1bcea58e5ef521b280cfbf2e3
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-17 12:16:06 -0300

    Bootstrap e router

---
+++ js/about.c0b66644.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcadastro_clientes_vue"]=self["webpackChunkcadastro_clientes_vue"]||[]).push([[443],{1168:function(t,e,n){n.r(e),n.d(e,{default:function(){return r}});var s=function(){var t=this;t._self._c;return t._m(0)},u=[function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],a=n(1001),c={},l=(0,a.Z)(c,s,u,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.c0b66644.js.map
